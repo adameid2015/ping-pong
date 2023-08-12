0a86b32be7d213a323ed05e577fd663745fd188e
adameid2015 <142124749+adameid2015@users.noreply.github.com>
2023-08-13 03:13:23 +0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "xgame":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+e e 7 e e e e 7 e e 7 e e e e 7 
+e e 7 e 7 e e 7 7 e 7 e e e e 7 
+e e e e e e e e e e e e 7 e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e d e e e e e e e 
+e e e e d e e e e e e e e d e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e d e d e e e 
+d e e e e e e e e e e e e e e e 
+e e e e e e e d e e e e e e e e 
+e e e e d e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
